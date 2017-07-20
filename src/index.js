971633763ef7f6b5551f3667d71f34e9b07e6dd3
Shaun Wang <Zhehao.Wang@gcu.edu>
2017-07-20 08:21:03 -0700

    Finish tabs

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import ArrowForward from 'material-ui/svg-icons/navigation/arrow-forward';
 import ArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
 import {fullWhite, cyan200} from 'material-ui/styles/colors';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { CSSTransitionGroup } from 'react-transition-group'
 
 injectTapEventPlugin();
 
@@ -28,26 +29,40 @@ const tileTypes = [2,0,0,0,1,0,0,0,0,0,
                     0,0,1,0,0,0,1,0,1,0,
                     1,0,0,0,1,0,1,0,0,0,
                     0,0,1,3,1,0,0,0,1,0];
-const stepsList = [[
-    [0],
-    [0,1],
-    [0,1,2],
-    [0,1,2,3],
-    [0,1,2],
-    [0,1],
-    [0],
-    [0,10],
-    [0,10,20],
-    [0,10,20,30]
-],
-[
-    [0],
-    [0,10],
-    [0,10,20],
-    [0,10,20,30],
-    [0,10,20,30,40],
-    [0,10,20,30,40,50]
-]
+const stepsList = [
+    [
+        [
+            [0],
+            [0,1],
+            [0,1,2],
+            [0,1,2,3],
+            [0,1,2],
+            [0,1],
+            [0],
+            [0,10],
+            [0,10,20],
+            [0,10,20,30]
+        ],
+        []
+    ],
+    [
+        [
+            [0],
+            [0,10],
+            [0,10,20],
+            [0,10,20,30],
+            [0,10,20,30,40],
+            [0,10,20,30,40,50]
+        ],
+        [
+            [0],
+            [0,10],
+            [10],
+            [10,20],
+            [10,20,30],
+            [20,30]
+        ]
+    ]
 ]
 
 class Tile extends React.Component {
@@ -66,7 +81,6 @@ class Tile extends React.Component {
     }
     isCurrent = () => this.props.id === this.props.currentStepsList[this.props.currentStepsList.length-1]
     isWall = () => this.props.type === 1
-    isLast = () => (this.props.lastLocation == this.props.id) && !(this.isFinish() || this.isStart())
     isStart = () => this.props.type === 2
     isFinish = () => this.props.type === 3
     
@@ -75,7 +89,7 @@ class Tile extends React.Component {
                 <Paper zDepth={this.isWall() ? 5 : 1} 
                     className={`col-sm-1 square text-center  ${this.isCurrent() ? 'animated pulse' : ''}`}
                     >
-                    <div  zDepth={1} className={`${this.setClass()} ${this.isLast() ? 'reverse-ripple' : ''}`}>
+                    <div  zDepth={1} className={this.setClass()}>
                         
                     </div>
                 </Paper>
@@ -117,21 +131,42 @@ class Button extends React.Component {
     }
 }
 
-const Step = (props) => {
-	return (
-  	<Paper>
-          {props.id}. {props.step}
-      </Paper>)
+class Step extends React.Component {
+    getCoordinate = () => {
+        let step = this.props.step.toString();
+        if (step.length == 1){
+            return ( step + " . 0")
+        } else if (step.length == 2){
+            return (step[1] + " . " + step[0])
+        } else {
+            return (step[2] + " . " + step[0,2])
+        }
+    }
+	render(){
+        return (
+            <Paper className="step">
+            {this.getCoordinate()}
+            </Paper>)
+    }
 }
 
-const Steps = (props) => {
-	return(
-        <div>
-        {props.currentStepsList.map((step, index) => 
-                <Step step={step} key={index} id={index}/>
-        )}
+class Steps extends React.Component {
+    getClass = () => this.props.currentStepsList.length <= 20 ? "col-sm-12" : "col-sm-6";
+	render(){
+        return(
+        <div className="row steps">
+            <CSSTransitionGroup
+                transitionName="step"
+                transitionEnterTimeout={500}
+                transitionLeaveTimeout={500}>
+                {this.props.currentStepsList.map((step, index) => 
+                <Step className={this.getClass()} step={step} key={index} id={index}/>
+                )}
+        
+            </CSSTransitionGroup>
         </div>
   	)
+    }
 }
 
 
@@ -166,21 +201,16 @@ class TabSwitch extends React.Component {
 
 class Dashboard extends React.Component{
     state = {
-        method: 1,
+        method: 0,
         currentStepNumber: 3,
         stepsList: stepsList,
-        previousLocaiton: null,
     };
-    retrieveCurrentStepsList = () => this.state.stepsList[this.state.method][this.state.currentStepNumber];
+    retrieveCurrentStepsList = () => this.state.stepsList[this.state.method][0][this.state.currentStepNumber];
+    retrieveCurrentDataStructure = () => this.state.stepsList[this.state.method][this.state.method][this.state.currentStepNumber]
   
     setStepNumber = (num) => {
-        this.setState(prevState => {
-            let prevList = prevState.stepsList[this.state.method];
-            let prevNum = prevState.currentStepNumber;
-            let prevSteps = prevList[prevNum];
-            return {currentStepNumber: num, 
-                    previousLocaiton: prevSteps[prevSteps.length-1]}
-        })
+        this.setState({currentStepNumber: num}
+        )
     };
 
     toggleMethod = () => {
@@ -192,12 +222,12 @@ class Dashboard extends React.Component{
     tiles = tileTypes.map((type, index) =>
           <Tile key={index} type={type} id={index} currentStepsList={this.retrieveCurrentStepsList()}/>);
   
-    getTotalNumberOfSteps = () => this.state.stepsList[this.state.method].length
+    getTotalNumberOfSteps = () => this.state.stepsList[this.state.method][0].length
 
    
 
     render(){
-  	var {currentStepNumber,method, previousLocaiton} = this.state;
+  	var {currentStepNumber,method} = this.state;
     return (
         <div className="dashboard">
         <div className="row">
@@ -217,12 +247,12 @@ class Dashboard extends React.Component{
                         totalNumberOfSteps={this.getTotalNumberOfSteps()}/>
                 </div>
                 {tileTypes.map((type, index) =>
-                        <Tile key={index} type={type} id={index} lastLocation={previousLocaiton}
+                        <Tile key={index} type={type} id={index}
                         currentStepsList={this.retrieveCurrentStepsList()}/>)}
                 </div>
             </div>
             <div className="col-sm-3 text-center pull-right">
-            <TabSwitch currentStepsList={this.retrieveCurrentStepsList()} setStepNumber={this.setStepNumber} method={method}
+            <TabSwitch currentStepsList={this.retrieveCurrentDataStructure()} setStepNumber={this.setStepNumber} method={method}
                 toggleMethod={this.toggleMethod} />
             </div>
         </div>
